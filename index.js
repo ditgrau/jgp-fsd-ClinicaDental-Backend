31f6198c3e6ddade419cc748a45236f1583a80f5
DitGrau <juditgraup@gmail.com>
2023-06-07 20:31:32 +0200

    FEAT: conexion con la base de datos

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
+const db = require('./db');
 
 
 app.get('/', (req, res) => {
     res.send('Hello World')
 })
 
-app.listen(PORT, ()=>{
-    console.log ('servidor levantado en el puerto ' + PORT)
+
+
+db.then (()=>{
+    app.listen(PORT, ()=>{
+        console.log ('servidor levantado en el puerto ' + PORT);
+    })
+})
+.catch ((error)=> {
+    console.error ('Error inicializando el servidor', error.message);
 })
