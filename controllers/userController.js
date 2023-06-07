c72ad74c56521dc96da5059306211f2199879f68
DitGrau <juditgraup@gmail.com>
2023-06-07 23:42:34 +0200

    FEAT: funcion getAllClients para los doctores

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,33 @@
-const { User } = require('..models');
+const { User } = require('../models');
 const userController = {}
 ///////////////////////////
 
-
+userController.getAllClients = async (req, res) => {
+    try {
+        const roleId = 1 //user 
+        const allClients = await User.findAll(
+            {
+                where: {
+                    role_id: roleId
+                },
+                attributes: {
+                    exclude: ["createdAt", "updatedAt", "password"]
+                }
+            }
+        )
+        return res.json({
+            sucess: true, 
+            message: "All results displayed",
+            allClients: allClients
+        })
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: 'Can not be displayed',
+            error: error.message
+        })
+        
+    }
+}
 ///////////////////////////
 module.exports = userController;
--- views/userRoutes.js
+++ views/userRoutes.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const userController = require('../controllers/userController');
 ///////////////////////////
 
+router.get('/clients', userController.getAllClients)
 
 ///////////////////////////
 module.exports = router;
