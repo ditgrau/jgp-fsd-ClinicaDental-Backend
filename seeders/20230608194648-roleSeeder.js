3f45b2c76108cd465bc1c1b808f168027b97d480
DitGrau <juditgraup@gmail.com>
2023-06-08 21:50:42 +0200

    FEAT: seeders role

---
+++ seeders/20230608194648-roleSeeder.js
@@ -0,0 +1,30 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert('Roles', [
+      {
+        id: 1,
+        name: "admin"
+      },
+      {
+        id: 2,
+        name: "doctor"
+      },
+      {
+        id: 3,
+        name: "user"
+      }
+    ])
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
