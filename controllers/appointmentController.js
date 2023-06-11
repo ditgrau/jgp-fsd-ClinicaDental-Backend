829677c0d527a352f870942aa4fa7df78fa94e6f
DitGrau <juditgraup@gmail.com>
2023-06-11 21:26:05 +0200

    FEAT: seeders para appointments

--- controllers/appointmentController.js
+++ controllers/appointmentController.js
@@ -3,26 +3,26 @@ const appoinmentController = {};
 
 /////////////////////////////////////////////////////////////
 
-appoinmentController.newAppoint = async (req, res) => {
-  try {
-    const myId = req.userId //lo saco del token
-    const {date, time} = req.body;
-    const newAppoint = await Appointment.create({
-        userId: myId,
-        dentistId: 3, //////no lo va a coger no tengo la tabla ni los seedersss
-        date: date,
-        time: time
-    })
+// appoinmentController.newAppoint = async (req, res) => {
+//   try {
+//     const myId = req.userId //lo saco del token
+//     const {date, time} = req.body;
+//     const newAppoint = await Appointment.create({
+//         userId: myId,
+//         dentistId: 3, //////no lo va a coger no tengo la tabla ni los seedersss
+//         date: date,
+//         time: time
+//     })
     
-  } catch (error) {
-    return res.status(500).json({
-      success: false, 
-      message: "Server error",
-      error: error.message
-    })
+//   } catch (error) {
+//     return res.status(500).json({
+//       success: false, 
+//       message: "Server error",
+//       error: error.message
+//     })
     
-  }
-}
+//   }
+// }
 
 
 /////////////////////////////////////////////////////////////
---
+++ seeders/20230611190701-appointmentSeeder.js
@@ -0,0 +1,67 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.bulkInsert('Appointments', [
+      {
+        id: 1,
+        userId: 8,
+        dentistId: 3,
+        date: '2023-06-07',
+        hour: '11:30:00'
+      },
+      {
+        id: 2,
+        userId: 7,
+        dentistId: 2,
+        date: '2024-01-17',
+        hour: '10:30:00'
+      },
+      {
+        id: 3,
+        userId: 6,
+        dentistId: 1,
+        date: '2023-11-27',
+        hour: '13:30:00'
+      },
+      {
+        id: 4,
+        userId: 6,
+        dentistId: 2,
+        date: '2023-10-10',
+        hour: '16:00:00'
+      },
+      {
+        id: 5,
+        userId: 7,
+        dentistId: 1,
+        date: '2023-07-09',
+        hour: '10:30:00'
+      },
+      {
+        id: 6,
+        userId: 8,
+        dentistId: 3,
+        date: '2023-06-08',
+        hour: '10:00:00'
+      },
+      {
+        id: 7,
+        userId: 8,
+        dentistId: 3,
+        date: '2023-09-12',
+        hour: '10:30:00'
+      }
+    ])
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
